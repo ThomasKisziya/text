7a11523e15e51a9fcc3b7b3f24757ff8d73d4e23
Thomas <aleexx7@protonmail.com>
2025-04-04 00:57:38 +0100

    Create text_reverser.py

---
+++ text_reverser.py
@@ -0,0 +1,10 @@
+# text_reverser.py
+
+# Function to reverse the entered text
+def reverse_text(text):
+    return text[::-1]
+
+# Main function to prompt the user for input
+def main():
+    print("Text Reverser - Reverse the Entered Text")
+    while True:
